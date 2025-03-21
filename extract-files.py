7309cf61b81b048a496d7a66c4d9989ac4b8cdc6
dianlujitao <dianlujitao@lineageos.org>
2025-03-21 11:18:16 +0800

    sm8650-common: Address seccomp failures for qsap_location
    Change-Id: Ia6da59107f1c81fc9af55bbf73d2252016fd17df

--- extract-files.py
+++ extract-files.py
@@ -124,6 +124,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/seccomp_policy/gnss@2.0-qsap-location.policy': blob_fixup()
+        .add_line_if_missing('sched_get_priority_min: 1')
+        .add_line_if_missing('sched_get_priority_max: 1'),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
         .add_needed('libhidlbase_shim.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()

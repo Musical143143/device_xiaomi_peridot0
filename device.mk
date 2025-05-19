902bd1848677ee82f667435da65baf56fc6b7620
koko-07870 <ko282ko82@gmail.com>
2025-05-19 20:15:22 +0530

    avalon: Initial device tree conversion
    Change-Id: I54af24af8721b5b60ae0862fcd0d29da0e7b7dd9

--- device.mk
+++ device.mk
@@ -14,8 +14,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 3216
-TARGET_SCREEN_WIDTH := 1440
+TARGET_SCREEN_HEIGHT := 2772
+TARGET_SCREEN_WIDTH := 1240
 
 # Display
 PRODUCT_COPY_FILES += \
@@ -64,4 +64,4 @@ PRODUCT_PACKAGES += \
 $(call inherit-product, device/oneplus/sm8650-common/common.mk)
 
 # Inherit from the proprietary files makefile.
-$(call inherit-product, vendor/oneplus/waffle/waffle-vendor.mk)
+$(call inherit-product, vendor/oneplus/avalon/avalon-vendor.mk)
--- lineage_avalon.mk
+++ lineage_avalon.mk
@@ -8,24 +8,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from waffle device
-$(call inherit-product, device/oneplus/waffle/device.mk)
+# Inherit from avalon device
+$(call inherit-product, device/oneplus/avalon/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_waffle
-PRODUCT_DEVICE := waffle
+PRODUCT_NAME := lineage_avalon
+PRODUCT_DEVICE := avalon
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2581
+PRODUCT_MODEL := CPH2661
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 15 AP3A.240617.008 1754899593570 release-keys" \
-    BuildFingerprint=OnePlus/CPH2581EEA/OP595DL1:15/AP3A.240617.008/U.R4T3.1ae84e1_4416c1_42b995:user/release-keys \
-    DeviceName=OP595DL1 \
-    DeviceProduct=CPH2581 \
-    SystemDevice=OP595DL1 \
-    SystemName=CPH2581
+    BuildDesc="qssi_64-user 15 AP3A.240617.008 1754565910728 release-keys" \
+    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:15/UKQ1.231108.001/U.R4T2.1f18640-1ecca-1ecc7:user/release-keys \
+    DeviceName=OP5E93L1 \
+    DeviceProduct=CPH2661 \
+    SystemDevice=OP5E93L1 \
+    SystemName=CPH2661

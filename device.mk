02f48da61516c2194adcd16974629977ea11baf4
Musical143143 <basub4615@gmail.com>
2026-08-11 07:01:21 +0000

    welcome evolution X

--- device.mk
+++ device.mk
@@ -38,8 +38,8 @@ $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-evolution
 PRODUCT_PACKAGES += \
     KeyHandlerResTarget \
     OPlusFrameworksResTarget \
